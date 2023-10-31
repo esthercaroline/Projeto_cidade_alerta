71a97ffbd0f63a8beeff51be40e18dc7c496f62f
laupontiroli <laurapm@al.insper.edu.br>
2023-10-31 17:16:49 -0300

    rota de mostrar problemas feita e começo do upload de fotos

--- app.py
+++ app.py
@@ -1,17 +1,50 @@
-from flask import Flask, request, jsonify
-from pymongo import MongoClient
-from bson import ObjectId
-import credentials_file as credentials
-app = Flask("Cidade Alerta")
+from flask import Flask, jsonify, request
+from flask_pymongo import PyMongo, ObjectId
+from credentials_file import settings, credentials
+from werkzeug.utils import secure_filename
+import os 
 
-# Conexão com o Mongo
-con_string = f"mongodb+srv://{credentials['user_mongo']}:{credentials['password_mongo']}@cidadealerta.2nltvs3.mongodb.net/"
-client = MongoClient(con_string)
+UPLOAD_FOLDER = '/photos'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+app = Flask(__name__)
+app.config["MONGO_URI"] = f"mongodb+srv://{credentials['user_mongo']}:{credentials['password_mongo']}@{settings['host']}/{settings['database']}?retryWrites=true&w=majority"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+mongo = PyMongo(app)
 
-col_problemas = client.db_cidade_alerta.problemas
+def allowed_foto(fotoname):
+    return '.' in fotoname and \
+           fotoname.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 # Rotas
 @app.route("/problemas", methods=["GET"])
 def get_problemas():
-    problemas = col_problemas.find()
-    return jsonify(problemas)
+    try:
+        filter_ = {}
+        projection_ = {}
+        problemas = list(mongo.db.problemas.find(filter_, projection_))
+        for problema in problemas:
+            problema["_id"] = str(problema["_id"])
+        return {"problemas": problemas}, 200
+    except Exception as e: 
+        return {'erro': f'{e}'}
+
+@app.route("/problemas", methods=["POST"])
+def cadastro_problemas():
+    try:
+        data = request.data
+        foto = request.files['foto']
+        if foto and allowed_foto(foto.filename):
+            filename = secure_filename(foto.filename)
+            foto.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        data['foto'] = filename
+        if not all(k in data for k in ("rua", "bairro", "problema_tipo","urgencia","problema_descricao","data_inicio")):
+            return jsonify({"erro": "Campos obrigatórios faltando!"}), 400
+        problema_id = mongo.db.problemas.insert_one(data)
+        print(problema_id.inserted_id)
+        return {"_id": str(problema_id.inserted_id)}, 201
+    except Exception as e: 
+        return {'erro': f'{e}'}
+
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True, port=4800)
