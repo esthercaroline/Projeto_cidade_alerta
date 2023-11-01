95a86cef5a25832161c88bb309104e4aaf778974
Esther <estherccr@al.insper.edu.com>
2023-11-01 15:48:12 -0300

    rota filtros nao sei se ta funcionando

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 UPLOAD_FOLDER = '/photos'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 app = Flask(__name__)
-app.config["MONGO_URI"] = f"mongodb+srv://{credentials['user_mongo']}:{credentials['password_mongo']}@{settings['host']}/{settings['database']}?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = f"mongodb+srv://{credentials['user_mongo']}:{credentials['password_mongo']}@{settings['host']}{settings['database']}?retryWrites=true&w=majority"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 mongo = PyMongo(app)
 
@@ -45,6 +45,17 @@ def cadastro_problemas():
     except Exception as e: 
         return {'erro': f'{e}'}
 
+@app.route("/problemas/filter", methods=["GET"])
+def get_problemas_filter():
+    try:
+        filter_ = request.args
+        projection_ = {}
+        problemas = list(mongo.db.problemas.find(filter_, projection_))
+        for problema in problemas:
+            problema["_id"] = str(problema["_id"])
+        return {"problemas": problemas}, 200
+    except Exception as e: 
+        return {'erro': f'{e}'}
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True, port=4800)
